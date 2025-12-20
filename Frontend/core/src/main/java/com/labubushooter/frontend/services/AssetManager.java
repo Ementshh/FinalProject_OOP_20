da7e2392cbdfaa713b32c250f9f944d7b8bf3518
Ementshh <clementheo.benaya@ui.ac.id>
2025-12-20 23:21:18 +0700

    Remove background redundancy

--- Frontend/core/src/main/java/com/labubushooter/frontend/services/AssetManager.java
+++ Frontend/core/src/main/java/com/labubushooter/frontend/services/AssetManager.java
@@ -46,7 +46,6 @@ public class AssetManager implements Disposable {
     public static final String MINI_BOSS = "miniboss";
     public static final String BOSS = "boss";
     public static final String PISTOL = "pistol";
-    public static final String BACKGROUND = "background";
     public static final String BACKGROUND_LEVEL1 = "background_level1";
     public static final String BACKGROUND_LEVEL2_TO_4 = "background_level2to4";
     public static final String BACKGROUND_LEVEL5 = "background_level5";
@@ -133,7 +132,6 @@ public class AssetManager implements Disposable {
         loadTexture(MINI_BOSS, "miniboss.png");
         loadTexture(BOSS, "boss.png");
         loadTexture(PISTOL, "pistol.png");
-        loadTexture(BACKGROUND, "background.png");
         loadTexture(BACKGROUND_LEVEL1, "bglevel1.png");
         loadTexture(BACKGROUND_LEVEL2_TO_4, "bglevel2to4.png");
         loadTexture(BACKGROUND_LEVEL5, "bglevel5.png");
